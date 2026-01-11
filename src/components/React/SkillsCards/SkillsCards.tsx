59491bca42cc84e0d5541f22e4c6515d98cb660c
Kenner <kennersteven4@gmail.com>
2026-01-11 14:06:19 -0500

    Button fixes

--- src/components/React/SkillsCards/SkillsCards.tsx
+++ src/components/React/SkillsCards/SkillsCards.tsx
@@ -8,7 +8,7 @@ export default function SkillsCards() {
         {ItemSkills.map((skill, k) => (
           <div
             key={k}
-            className="col-lg-3 col-md-3 col-sm-4 col-5 d-flex justify-content-center m-1"
+            className="col-lg-2 col-md-3 col-sm-4 col-5 d-flex  m-1"
           >
             <CardSkill
               description={skill.description}
